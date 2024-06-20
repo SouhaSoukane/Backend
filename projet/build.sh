836c2e6d81eed2e5010ec375432ad6d77d2c8173
SouhaSoukane <90262883+SouhaSoukane@users.noreply.github.com>
2024-06-20 17:43:12 +0200

    Update build.sh

--- projet/build.sh
+++ projet/build.sh
@@ -1,11 +1,11 @@
 set -o errexit
-REQUIREMENTS_FILE="./env/requirements.txt"
+REQUIREMENTS_FILE="./venv/requirements.txt"
 
 # Install dependencies from requirements.txt
 pip install -r "$REQUIREMENTS_FILE"
 
 # Navigate to the directory containing manage.py
-cd Projet_2CS/NewEsi/
+cd projet/projet
 
 # Collect static files
 python manage.py collectstatic --no-input
