51d60c7097b6d037949e30a40e548b794211fdc4
SouhaSoukane <90262883+SouhaSoukane@users.noreply.github.com>
2024-06-20 17:41:02 +0200

    Update build.sh

--- projet/build.sh
+++ projet/build.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
+REQUIREMENTS_FILE="./env/requirements.txt"
 
-pip install -r requirements.txt
+# Install dependencies from requirements.txt
+pip install -r "$REQUIREMENTS_FILE"
 
+# Navigate to the directory containing manage.py
+cd Projet_2CS/NewEsi/
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
